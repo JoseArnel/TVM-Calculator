4b6c9088c4b9c6547fe8a362955100ccb8d76516
Jose Arnel <jonel.manipon@gmail.com>
2021-12-04 14:59:46 -0500

    more_task_interest_setup

--- tvm2.py
+++ tvm2.py
@@ -1,17 +1,9 @@
+pv = int(input("Present Value: "))
 fv = int(input("Future Value: "))
-i = float(input("Interest(%): "))/100
 n = int(input("Number of Periods: "))
 pmt = int(input("Payment: "))
-for x in range(0,n):
-    pv = (fv - pmt) / (1 + i)
-    p_i= pv * i
-    print("Period:" + str(x) +  " FV:{:.2f}".format(fv) + " PMT:" + str(pmt) + " Interest:{:.2f}".format(p_i) + " PV:{:.2f}".format(pv))
-    fv = pv
-print("Present Value = ${:.2f}".format(pv))
+save = input("Would you wanna save the values? (y/n) ")
+i = (((fv / pv)**(1/n)) - 1) * 100
+print("Interest: {:0.3f}%".format(i))
 
-# fv = pv + pmt + (pv*i)
-# pv = fv - pmt - (pv*i)
-# pv + (pv*i) = fv - pmt (- (pv*i) + (pv*i))
-# pv(1 + i ) = fv - pmt (- (pv*i) + (pv*i))
-# pv(1 + i) / (1 + i) = (fv - pmt) / (1 + i)
-# pv = (fv - pmt) / (1 + i)
+
