e644c194bc703a66fe65aeebb25d0f5e19178dce
Jose Arnel <jonel.manipon@gmail.com>
2023-03-10 16:27:56 -0500

    tvm_visualize_unfinished

--- TFSA_Cont.py
+++ TFSA_Cont.py
@@ -2,6 +2,26 @@ import math
 from os import system, name
 import datetime
 import csv
+import pandas as pd
+from matplotlib import pyplot as plt
+
+def interest_visualizer():
+    data = pd.read_csv('investment.csv')
+    plt.plot(data.Balance / 10**6)
+    plt.plot(data.Principal / 10**6)
+    plt.plot(data.Interest / 10**6)
+    plt.legend(['Balance', 'Interest', 'Principal'])
+    plt.xlabel("Year")
+    plt.ylabel("$(Millions)")
+    plt.show()
+
+def tvm_visualizer():
+    data = pd.read_csv('tvm.csv')
+    plt.plot(data.PV / 10**6)
+    plt.plot(data.PMT / 10**6)
+    plt.plot(data.Interest / 10**6)
+    plt.plot(data.FV / 10**6)
+    plt.show()
 
 def write_csv(n, year, value):
   with open('investment.csv', 'w', newline='') as file:
@@ -66,7 +86,7 @@ def write_csv(n, year, value):
       writer.writerow([year, value,])
 
 def FutureValue(calc, pv, i, n, pmt):
-  with open('investment.csv', 'w', newline='') as file:
+  with open('interest.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["Year", "Balance", "Principal", "Interest"])
     principal_s = pv 
@@ -92,16 +112,42 @@ def FutureValue(calc, pv, i, n, pmt):
             fv = pv + p_i - pmt
             pv = fv
     return(fv)
-    
+
+def FutureValue2(calc, pv, i, n, pmt):
+  with open('tvm.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Year", "PV", "PMT", "Interest", "FV"])
+    principal_s = pv 
+    balance_s = pv 
+    principal_e = pv
+    clear()
+    if (calc == 'y'):
+        for x in range(1,n+1):
+            p_i= pv * i
+            fv = pv + p_i - pmt
+            principal_e = principal_s - pmt
+            print("   start_principal " + " start_balance" +   " interest " + " end_balance " + " end_principal ")
+            print(str(x) + " " +"{:.2f}".format(principal_s) +  "        " + "{:.2f}".format(balance_s) +  "     " + "{:.2f}".format(p_i)  + "  " + "{:.2f}".format(fv) +  "    " + "{:.2f}".format(principal_e))
+            # print("principal:{:.2f}".format(principal_s) + "start balance::{:.2f}".format(balance_s) +   " interest:{:.2f}".format(p_i) + "end balance:{:.2f}".format(fv) + "end balance:{:.2f}".format(principal_e))
+            writer.writerow([x, round(pv), round(pmt), round(p_i), round(fv)])
+            principal_s = principal_s - pmt
+            balance_s = fv
+            pv = fv
+        print(" ")
+    else:
+        for x in range(1,n+1):
+            p_i= pv * i
+            fv = pv + p_i - pmt
+            pv = fv
+    return(fv)
+        
 def InterestFutureValue(pv, i, n, pmt):
     sum = -pv
     for y in range(1, n+1):
         sum += pmt
     return(-1*sum)
 
-# write_csv()
-
-Milli()
+tvm_visualizer()
 
   
 
