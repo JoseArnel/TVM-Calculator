50d8bcf679854be6cd1efad265c2091770e321b0
Jose Arnel <jonel.manipon@gmail.com>
2023-03-15 14:18:13 -0400

    acc/sum

--- TFSA_Cont.py
+++ TFSA_Cont.py
@@ -73,7 +73,7 @@ def Milli():
     count = 0
     for x in range(100):
         count += 1
-        fv = FutureValue2(calc, pv, i, count, pmt)
+        fv = FutureValue3(calc, pv, i, count, pmt)
         # interest = InterestFutureValue(pv, i, count, pmt)
         if (fv > target):
             print("It will take you " + str(count) + "years to be a millionaire by: " + str(count+18) + " \n")
@@ -115,7 +115,6 @@ def FutureValue(calc, pv, i, n, pmt):
             pv = fv
     return(fv)
 
-
 # sum of interests
 # total interests
 #Sum of all periodic payments
@@ -134,8 +133,39 @@ def FutureValue2(calc, pv, i, n, pmt):
             principal_e = principal_s - pmt
             print("   start_principal " + " start_balance" +   " interest " + " end_balance " + " end_principal ")
             print(str(x) + " " +"{:.2f}".format(principal_s) +  "        " + "{:.2f}".format(balance_s) +  "     " + "{:.2f}".format(p_i)  + "  " + "{:.2f}".format(fv) +  "    " + "{:.2f}".format(principal_e))
-            # print("principal:{:.2f}".format(principal_s) + "start balance::{:.2f}".format(balance_s) +   " interest:{:.2f}".format(p_i) + "end balance:{:.2f}".format(fv) + "end balance:{:.2f}".format(principal_e))
             writer.writerow([x, round(pv), round(-pmt), round(p_i), round(-fv)])
+            principal_s = principal_s - pmt
+            balance_s = fv
+            pv = fv
+        print(" ")
+    else:
+        for x in range(1,n+1):
+            p_i= pv * i
+            fv = pv + p_i - pmt
+            pv = fv
+    return(fv)
+        
+def FutureValue3(calc, pv, i, n, pmt):
+  with open('tvm.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Year", "PV", "Sum of PMT", "Accumulated Interest", "FV"])
+    principal_s = pv 
+    balance_s = pv 
+    principal_e = pv
+    sum_pmt = 0
+    sum_int = 0
+
+    clear()
+    if (calc == 'y'):
+        for x in range(1,n+1):
+            p_i= pv * i
+            fv = pv + p_i - pmt
+            principal_e = principal_s - pmt
+            print("   start_principal " + " start_balance" +   " interest " + " end_balance " + " end_principal ")
+            print(str(x) + " " +"{:.2f}".format(principal_s) +  "        " + "{:.2f}".format(balance_s) +  "     " + "{:.2f}".format(p_i)  + "  " + "{:.2f}".format(fv) +  "    " + "{:.2f}".format(principal_e))
+            sum_pmt = pmt + sum_pmt
+            sum_int = p_i + sum_int
+            writer.writerow([x, round(pv), round(-sum_pmt), round(sum_int), round(-fv)])
             principal_s = principal_s - pmt
             balance_s = fv
             pv = fv
@@ -153,7 +183,7 @@ def InterestFutureValue(pv, i, n, pmt):
         sum += pmt
     return(-1*sum)
 
-tvm_visualizer()
+Milli()
 
   
 
